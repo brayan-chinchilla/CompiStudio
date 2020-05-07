46b5a2dc202fcd14c4510a47ef19d15ae7d904c8
Brayan Chinchilla <brayanch1297@gmail.com>
2020-05-07 11:07:30 -0600

    front and back com

--- frontend/controller.js
+++ frontend/controller.js
@@ -1,79 +1,110 @@
-var console = CodeMirror.fromTextArea(document.getElementById('console_Editor'), {
-  value: "function myScript(){return 100;}\n",
-  lineNumbers : true,
-  firstLineNumber: 1,
-  mode:  "javascript",
-  theme: "material-darker",
-});
+var cs_symbolTable;
+var cs_C3D_Editor;
+var cs_C3D_Optimizado_Editor;
+var cs_consoleEditor;
 
-var jEditor2 = CodeMirror.fromTextArea(document.getElementById('c3D_Editor1'), {
-  value: "function myScript(){return 100;}\n",
-  lineNumbers : true,
-  firstLineNumber: 1,
-  mode:  "javascript",
-  theme: "material-darker",
-});
+function initStaticEditors(){
+  cs_consoleEditor = CodeMirror.fromTextArea(document.getElementById('console_Editor'), {
+    lineNumbers : true,
+    mode:  "javascript",
+    theme: "material-darker",
+  });
+  cs_consoleEditor.refresh();
 
-jEditor2.setValue(`function func1(a, b, c = 3){
-let arr;
-arr[2] = true || false;
-for(var i = 0; i < 5; i++){
-  global b = 5 * 2 + "instance";
+  cs_C3D_Editor = CodeMirror.fromTextArea(document.getElementById('C3D_Editor'), {
+    lineNumbers : true,
+    mode:  "javascript",
+    theme: "material-darker",
+  });
+  cs_C3D_Editor.refresh();
+
+  cs_C3D_Optimizado_Editor = CodeMirror.fromTextArea(document.getElementById('C3D_Optimizado_Editor'), {
+    lineNumbers : true,
+    mode:  "javascript",
+    theme: "material-darker",
+  });
+  cs_C3D_Optimizado_Editor.refresh();
 }
-}`);
 
-
-
-$(document).ready(function() {
-  $('#example').DataTable({
+function initSymbolTable(){
+  cs_symbolTable = $('#table_SymbolTable').DataTable({
     info: false,
     paging: false,
     scrollY: '30vh',
     fixedHeader: {
       header: true,
       footer: false
-  }
+    }
   });
-} );
-
-
-function openFile() {
-  var file = document.getElementById('fileInput').files[0];
-  if (file) {
-      var fileReader = new FileReader();
-      fileReader.onload = function (fileLoadedEvent) {
-          var textFromFileLoaded = fileLoadedEvent.target.result;
-          //let number = activeTab.split(' ');
-          jEditor.setValue(textFromFileLoaded);
-      };
-      fileReader.readAsText(file, "UTF-8");
-  } else {
-  }
 }
 
-newFileS;
-
 $( function() {
-  var tabTitle = $( "#tab_title" ),
-    tabContent = $( "#tab_content" ),
-    tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
-    tabCounter = 2;
+  initStaticEditors();
+  initSymbolTable();
+  initEditorTabs();
+} );
+
+function initEditorTabs(){
+
+  $("#jEditor_FileJ_1").data('CodeMirrorInstance',
+    CodeMirror.fromTextArea(document.getElementById("jEditor_FileJ_1"), {
+      lineNumbers : true,
+      mode:  "javascript",
+      theme: "material-darker",
+    })
+  );
+
+  var tabTemplate = "<li><a href='#{href}'>#{label}</a><span class='ui-icon ui-icon-close' role='presentation'>x</span></li>";
+  var tabCounter = 2;
 
   var tabs = $( "#tabs" ).tabs();
 
   // Actual addTab function: adds new tab using the input from the form above
-  function newFile() {
-    var label = tabTitle.val() || "Tab " + tabCounter,
-      id = "tabs-" + tabCounter,
-      li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
-      tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+  $('#newFileMenu').click(function() {
+    var label = "FileJ_" + tabCounter;
+    var id = "FileJ_" + tabCounter;
+    var li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) );
 
     tabs.find( ".ui-tabs-nav" ).append( li );
-    tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+
+    tabs.append(`
+      <div id="${id}" style="height:90%">
+      <textarea style="width: 100%; height:100%" id="jEditor_${id}"></textarea>
+      </div> 
+    `)
+
     tabs.tabs( "refresh" );
+    tabs.tabs( "option", "active", tabs.find(".ui-tabs-nav > li").length - 1);
     tabCounter++;
-  };
-  newFileS = newFile;
+
+    var jEditor = CodeMirror.fromTextArea(document.getElementById("jEditor_" + id), {
+      lineNumbers : true,
+      mode:  "javascript",
+      theme: "material-darker",
+    });
+
+    $("#jEditor_" + id).data('CodeMirrorInstance', jEditor);
+    jEditor.refresh();
+  });
+
+  $('#fileInput').change(function() {
+    var file = document.getElementById('fileInput').files[0];
+    if (file) {
+        var fileReader = new FileReader();
+        fileReader.onload = function (fileLoadedEvent) {
+            var textFromFileLoaded = fileLoadedEvent.target.result;
+
+            $('#newFileMenu').click();
+            var editorId = "jEditor_" + tabs.find(".ui-tabs-nav > li:last-child").attr('aria-controls');
+
+            var editor = $("#" + editorId).data('CodeMirrorInstance');
+            editor.setValue(textFromFileLoaded);
+        };
+        fileReader.readAsText(file, "UTF-8");
+    } else {
+      alert("No se pudo abrir el archivo")
+    }
+  });
 
   // Close icon: removing the tab on click
   tabs.on( "click", "span.ui-icon-close", function() {
@@ -81,4 +112,43 @@ $( function() {
     $( "#" + panelId ).remove();
     tabs.tabs( "refresh" );
   });
-} );
+}
+
+function compile(){
+  var focusedTab =  $( "#tabs" ).tabs( "option", "active" ) + 1;
+
+  var editorId = "jEditor_" + $("#tabs").find(`.ui-tabs-nav > li:nth-child(${focusedTab})`).attr('aria-controls');  
+  var editor = $("#" + editorId).data('CodeMirrorInstance');
+
+  httpPost('/compile', {sourceCode: editor.getValue()}, (response) => {
+    cs_C3D_Editor.setValue(response.C3D);
+  })
+}
+
+function optimize(){
+  httpPost('/optimize', {C3D: cs_C3D_Editor.getValue()}, (response) => {
+    cs_C3D_Optimizado_Editor.setValue(response.C3D_Optimizado)
+  })
+}
+
+function execute(){
+  httpPost('/execute', {C3D: cs_C3D_Editor.getValue()}, (response) => {
+    cs_consoleEditor.setValue(response.console);
+  })
+}
+
+function httpPost(url, obj, callback){
+  var http = new XMLHttpRequest();
+  http.open("POST", url, true);
+  http.setRequestHeader("Content-Type", "application/json");
+  http.onreadystatechange = function(){
+    if(http.readyState == 4){
+      if(http.status == 200){
+        callback(JSON.parse(http.responseText));
+      }else{
+        alert("Error en el servidor, ver consola")
+      }
+    }
+  }
+  http.send(JSON.stringify(obj));
+}
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var server = express();
 
+server.use(express.json())
+
 server.get('/', function(req, res){
     res.sendFile('frontend/index.html', {root: __dirname});
+})
+
+server.post('/compile', function(req, res){
+    console.log(req.body.sourceCode);
+    res.send({
+        C3D: "Codigo 3D"
+    });
+})
+
+server.post('/optimize', function(req, res){
+    console.log(req.body.C3D);
+    res.send({
+        C3D_Optimizado: "Optimizado"
+    })
+})
+
+server.post('/execute', function(req, res){
+    console.log(req.body.C3D);
+    res.send({
+        console: "Consola"
+    })
 })
 
 server.use(express.static(__dirname + '/frontend'));
